7fa54173a20631ecda1e8d3d9e8cc6d97027a4dc
jisanghoon <246060@naver.com>
2016-12-26 16:14:24 +0900

    테스트 완료

---
+++ src/main/java/kr/or/dgit/bigdata/school/TestMain.java
@@ -0,0 +1,14 @@
+package kr.or.dgit.bigdata.school;
+
+import javax.swing.JFrame;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		JFrame jf=new JFrame();
+		jf.setTitle("테스트");
+		jf.setSize(500, 500);
+		jf.setVisible(true);
+	}
+
+}
